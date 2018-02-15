aaf3d88d27a5b4a36ddffff9fbd3ef4f0f603f9d
Eksa <eksoct@yahoo.com>
2018-02-15 10:51:15 +0700

    Login FB Done

--- assets/js/h8gag.js
+++ assets/js/h8gag.js
@@ -1,68 +1,56 @@
-const serverAPI = 'http://localhost:3000'
+const serverAPI = 'http://h8.awtian.com:3000'
 
-// window.fbAsyncInit = function () {
-//   var token = localStorage.getItem('accessToken')
-//   console.log('token: ', token);
-//
-//   // if (token != null) {
-//   // 	$(window).attr('location', '/index.html')
-//   // }
-//   // else {
-//     FB.init({
-//       appId: '389643448127142',
-//       // cookie: true,
-//       xfbml: true,
-//       version: 'v2.11'
-//     });
-//     FB.AppEvents.logPageView();
-//   // }
-// };
-//
-// (function (d, s, id) {
-//   var js, fjs = d.getElementsByTagName(s)[0];
-//   if (d.getElementById(id)) return;
-//   js = d.createElement(s); js.id = id;
-//   js.src = "https://connect.facebook.net/en_US/sdk.js";
-//   fjs.parentNode.insertBefore(js, fjs);
-// }(document, 'script', 'facebook-jssdk'));
-//
-// function checkLoginState() {
-//   FB.getLoginStatus(function (response) {
-//     if (response.status === 'connected') {
-//       localStorage.accessToken = response.authResponse.accessToken
-//       localStorage.userId      = response.authResponse.userID
-//       console.log('accessToken: ', response.authResponse.accessToken);
-//       sendTokenToServer(response.authResponse.accessToken);
-//     }
-//     else {
-//       localStorage.removeItem("accessToken")
-//       localStorage.removeItem("userId")
-//       localStorage.removeItem("jwt")
-//     }
-//   });
-// };
-//
-// function sendTokenToServer(token) {
-//   $.ajax({
-//     type: 'GET',
-//     url: serverAPI + '/user',
-//     headers: { access_token: token },
-//     success: function (resp) {
-//       FB.logout(function (response) {
-//         localStorage.jwt = resp.token
-//         // $(window).attr('location', '/index.html')
-//       });
-//     },
-//     error: function (error) {
-//       console.error('Failed to login with Facebook', error);
-//     }
-//   })
-// };
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: '389643448127142',
+    // cookie: true,
+    xfbml: true,
+    version: 'v2.11'
+  });
+  FB.AppEvents.logPageView();
+};
+
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function checkLoginState() {
+  FB.getLoginStatus(function (response) {
+    if (response.status === 'connected') {
+      localStorage.accessToken = response.authResponse.accessToken
+      localStorage.userId      = response.authResponse.userID
+      sendTokenToServer(localStorage.accessToken);
+    }
+    else {
+      localStorage.removeItem("accessToken")
+      localStorage.removeItem("userId")
+      localStorage.removeItem("jwt")
+    }
+  });
+};
+
+function sendTokenToServer(tokenFB) {
+  axios.post(serverAPI + '/user', {token: tokenFB})
+  .then(resp => {
+    localStorage.token = resp.data.jwt
+  })
+  .catch(err => {
+    console.log(err.message);
+  })
+};
 
 var app = new Vue({
   el: '#app-h8gag',
   data: {
     posts: [],
+    posting: {
+      image : '',
+      title : ''
+    }
   },
 
   created() {
@@ -71,54 +59,18 @@ var app = new Vue({
 
   methods: {
     getPosts() {
-      axios.get('http://server.wizawt.com:3000/posts')
+      axios.get(serverAPI + '/posts')
       .then(resp => {
-        // console.log(resp.data.data[0].image);
-        let arrData = []
-        let datas = resp.data.data
-        datas.map(data => {
-          // console.log(data._id);
-          var div = `<div class="animate-box">
-              <a :href="post.image" class="image-popup fh5co-board-img">
-                <img src="${data.image}" alt="">
-              </a>
-              <div class="fh5co-desc">${data.title}</div>
-            </div>`
-          arrData.push(div)
-          // arrData.push({
-          //   id: data._id,
-          //   title : data.title,
-          //   image: require(data.image),
-          // })
-        })
-        // console.log(arrData);
-
-        // this.posts = arrData
         this.posts = resp.data.data
       })
+    },
+    submitData() {
+      console.log('POSTING: ', JSON.stringify(this.posting));
+      this.$http.post(serverAPI + '/posts', headers:{ token })
+      .then(resp => {
+        console.log(resp);
+      })
     }
-  },
-
-  mounted() {
-    // window.fbAsyncInit = function() {
-    //     this.checkLogged();
-    //     FB.init({
-    //       appId     : '389643448127142',
-    //       cookie    : true,
-    //       xfbml     : true,
-    //       version   : 'v2.11'
-    //     });
-    //     FB.AppEvents.logPageView();
-    //   };
-
-      // (function(d, s, id){
-      //    var js, fjs = d.getElementsByTagName(s)[0];
-      //    if (d.getElementById(id)) {return;}
-      //    js = d.createElement(s); js.id = id;
-      //    js.src = "//connect.facebook.net/en_US/sdk.js";
-      //    fjs.parentNode.insertBefore(js, fjs);
-      //  }(document, 'script', 'facebook-jssdk'));
-
   }
 
 })
